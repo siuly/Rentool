1d193337591de608ed6eb7b28753867d285c57bf
Koichi <koichi89592@gmail.com>
2022-02-17 11:10:43 -0800

    Add basic return-tool page files

---
+++ Rentool/js/return-tool.js
@@ -0,0 +1,30 @@
+const sampleReservationData = {
+  reservationId: '1234556',
+  userId: '9sFhSueVxw3w8fniYAlE',
+  toolId: 'WEwfy9lVcUk6lXlJtYHj',
+  duration: {
+    startDate: '2022-02-20 13:30:00',
+    endDate: '2022-02-28 13:30:00'
+  },
+  isReturned: false,
+  reservationToolIndex: 'driver-brand-small'
+};
+
+const sampleLocationData = [{
+  locationId: '123456',
+  address: '100 W 49th Ave Vancouver, BC V5Y 2Z6',
+  latitude: '49.225518864967654',
+  longitude: '123.10776583584949',
+}, {
+  locationId: 'asdfgh',
+  address: ' 3211 Grant McConachie Way, Richmond, BC V7B 0A4',
+  latitude: '49.1967',
+  longitude: '123.1815',
+}];
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+  /**@type {[]} */
+  const reservationData = sampleReservationData;
+  const locations = sampleLocationData;
+});
