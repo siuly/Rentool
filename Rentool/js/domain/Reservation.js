c4f7ce61c92da3155b32bd482e8819cdf6b221da
Koichi <koichi89592@gmail.com>
2022-02-23 14:06:01 -0800

    Change reservation class

--- Rentool/js/domain/Reservation.js
+++ Rentool/js/domain/Reservation.js
@@ -1,10 +1,11 @@
 export class Reservation {
-  constructor({ reservationId, duration, isReturned, reservationToolIndex, toolId, userId }) {
+  constructor({ reservationId, duration, isReturned, reservationToolIndex, toolId, userId, toolName }) {
     this.reservationId = reservationId;
     this.duration = duration;
     this.isReturned = isReturned;
     this.reservationToolIndex = reservationToolIndex;
     this.toolId = toolId;
     this.userId = userId;
+    this.toolName = toolName;
   }
 }
