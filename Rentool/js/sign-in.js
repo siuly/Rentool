bfc4a5ca4375b7bdd481b80bb6df8bbdf38c9c71
TarasKing <ivanovtaras@live.ru>
2022-02-24 20:01:07 -0800

    fisnish 24feb

---
+++ Rentool/js/sign-in.js
@@ -0,0 +1,12 @@
+const login = document.getElementById('sign-in-btn');
+let singInEmailWithPassword = '';
+
+login.addEventListener('click', ()=> {
+  
+  const userEmail = document.getElementById('email');
+  const userPassword = document.getElementById('pswd');
+
+  // singInEmailWithPassword(userEmail, userPassword);
+
+  //  alert(`user email is ${userEmail} and user password is ${userPassword}`);
+});
