80546adcea908fa63aa2e47904ebf6e911fc5007
Koichi <koichi89592@gmail.com>
2022-02-25 21:10:39 -0800

    Change product-result page

--- Rentool/js/components/ToolListItem.js
+++ Rentool/js/components/ToolListItem.js
@@ -7,7 +7,9 @@ export class ToolListItem extends HTMLElement {
     this.render();
   }
   render() {
-    const { toolName, description, imageUrl, reservationToolIndex } = this.tool;
+    const { toolName, description, imageUrl, reservationToolIndex, isReserved } = this.tool;
+    console.log('isReserved: ', isReserved);
+    const EXCERPT_LENGTH = 50;
 
     this.classList.add('tool-list__item');
     this.innerHTML = `
@@ -15,17 +17,37 @@ export class ToolListItem extends HTMLElement {
         <img src="${imageUrl}" alt="${toolName}" class="tool-item__image">
       </div>
       <div class="tool-item__tool-name">${toolName}</div>
-      <div class="tool-item__description">${description}</div>
+
       <div class="tool-item__availability">
-        <span class="circle">
-        <i class="fa-solid fa-circle"></i></span><span class="tool-item__availability--text">Available at your location</span>
+        <span class="circle${isReserved ? ' reserved' : ''}">
+        <i class="fa-solid fa-circle"></i></span><span class="tool-item__availability--text">${
+          isReserved ? 'Unavailable at your location' : 'Available at your location'
+        }</span>
       </div>
       <form action="view-product.html" method="get">
         <input type="hidden" name="reservationToolIndex" value="${reservationToolIndex}">
-        <input type="submit" value="Button" class="tool-item__submit">
+        <input type="submit" value="Button" class="tool-item__submit" ${isReserved ? 'disabled' : ''}>
       </form>
     `;
   }
 }
 
 window.customElements.define('tool-list__item', ToolListItem);
+
+
+
+// this.innerHTML = `
+// <div class="tool-item__image-container">
+//   <img src="${imageUrl}" alt="${toolName}" class="tool-item__image">
+// </div>
+// <div class="tool-item__tool-name">${toolName}</div>
+// <div class="tool-item__description">${description.slice(0, EXCERPT_LENGTH)}</div>
+// <div class="tool-item__availability">
+//   <span class="circle">
+//   <i class="fa-solid fa-circle"></i></span><span class="tool-item__availability--text">Available at your location</span>
+// </div>
+// <form action="view-product.html" method="get">
+//   <input type="hidden" name="reservationToolIndex" value="${reservationToolIndex}">
+//   <input type="submit" value="Button" class="tool-item__submit">
+// </form>
+// `;
