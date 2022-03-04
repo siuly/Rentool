75ca8aec67c304a79f1874150d88f82b6c9ef17f
Koichi <koichi89592@gmail.com>
2022-03-03 19:14:54 -0800

    Change home page

--- Rentool/js/common.js
+++ Rentool/js/common.js
@@ -1,11 +1,1 @@
-// Header components
-const headerScript = document.createElement('script');
-headerScript.setAttribute('src', `/js/components/RentoolHeader.js`);
-headerScript.setAttribute('type', 'module');
-document.head.appendChild(headerScript);
-
-// Footer components
-const footerScript = document.createElement('script');
-footerScript.setAttribute('src', `/js/components/RentoolFooter.js`);
-footerScript.setAttribute('type', 'module');
-document.head.appendChild(footerScript);
+console.log("Common.js");
