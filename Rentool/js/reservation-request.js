af8d7ae682218e3136dd529d548225ad59965e1b
siuly <siulygt@gmail.com>
2022-03-04 14:29:52 -0800

    changes  for  second page

--- Rentool/js/reservation-request.js
+++ Rentool/js/reservation-request.js
@@ -56,7 +56,10 @@ let reservationRequestData = {
   createdAt: new Date()
   //You can add additional property
 };
+//elements from HTML
+
 let selectedLocation = null;
+let page1 = document.getElementById('page1');
 let sdate = document.getElementById('start-date');
 let stime = document.getElementById('start-time');
 let edate = document.getElementById('end-date');
@@ -65,14 +68,32 @@ let tname = document.getElementById('tool-namepg1');
 let tduration = document.getElementById('durationpg1');
 let tprice = document.getElementById('price');
 let tdepo = document.getElementById('deposit');
-tname.innerHTML = sampleToolData[0].toolName;
+let nextbtn = document.getElementById('page1-btn');
+let page2 = document.getElementById('page2');
+let tname2 = document.getElementById('tool-namepg2');
+let totalprice = document.getElementById('pricepg2');
+let duration = document.getElementById('durationpg2');
+let rentdays = document.getElementById('rental-dates');
+let tnameloc = document.getElementById('tool-name-location');
+let locationpicked = document.getElementById('location-selected');
+let payment = document.getElementById('method');
+let reservebtn = document.getElementById('page2-btn');
+let page3 = document.getElementById('page3');
+let page4 = document.getElementById('page4');
+let page5 = document.getElementById('page5');
+
+
 document.addEventListener('DOMContentLoaded', async () => {
 //@TODO: change variable name, this data is no longer sample
 const sampleToolData = await getToolsByReservationToolIndex(reservationToolIndex);
-  // console.log(sampleToolData[0].toolName);
-  etime.addEventListener('change', (event) => {
-    let dateinput1 = sdate.value + ' ' + stime.value;
-    let dateinput2 = edate.value + ' ' + etime.value;
+
+tname.innerHTML = sampleToolData[0].toolName;
+let dateinput1 = 0;
+let dateinput2 = 0;
+
+etime.addEventListener('change', (event) => {
+    dateinput1 = sdate.value + ' ' + stime.value;
+    dateinput2 = edate.value + ' ' + etime.value;
     console.log(dateinput1);
     console.log(dateinput2);
     if (dateinput1 < dateinput2) {
@@ -80,36 +101,79 @@ const sampleToolData = await getToolsByReservationToolIndex(reservationToolIndex
     } else {
       alert('incorrect date input');
     }
+    
   })
   
-  // with delta declare outside the function  I'll be able to use it for  other  sections
+
+  let diffHoursMilliseconds = 0;
+  let days = 0;
+  let fprice = 0;
+  let fdepo = 0;
   let delta = 0;
   function get_time_diff(d1, d2) {
     let datetime3 = new Date(d1);
     let datetime4 = new Date(d2);
-    let delta = datetime4 - datetime3;
+    delta = datetime4 - datetime3;
     console.log(delta);
-    let days = Math.floor(delta / 1000 / 60 / (60 * 24));
+    days = Math.floor(delta / 1000 / 60 / (60 * 24));
     console.log('days: ', days);
     let date_diff = new Date(delta);
     const diffMilliseconds = date_diff;
-    const diffHoursMilliseconds = Math.floor((diffMilliseconds - (days * 86400000))/3600000);
+    diffHoursMilliseconds = Math.floor((diffMilliseconds - (days * 86400000))/3600000);
     
-
-    tduration.innerHTML = `Duration: ${days} Days ${diffHoursMilliseconds} Hours`;
-    console.log(sampleToolData[0].prices.daily);
-    let fprice = sampleToolData[0].prices.daily * days + sampleToolData[0].prices.hourly * diffHoursMilliseconds;
-    let dailyp = sampleToolData[0].prices.daily * days;
-    let  hourlyp = sampleToolData[0].prices.hourly * diffHoursMilliseconds;
-    console.log(dailyp);
-    console.log(hourlyp);
-    tprice.innerHTML = `Price: $ ${fprice}`;
-    let fdepo = fprice * 0.5;
-    tdepo.innerHTML = `Deposit: $ ${fdepo}`;
-
-    return days + " Days " + date_diff.getHours() + " Hours " + date_diff.getMinutes() + " Minutes " + date_diff.getSeconds() + " Seconds";
-    // return delta;
+    let totalHours = 0;
+    let fixedhours = 4;
+    if (days == 0 && diffHoursMilliseconds < 4){
+      tduration.innerHTML = `Duration: ${days} Days ${diffHoursMilliseconds} Hours`;
+      fprice = sampleToolData[0].prices.hourly * fixedhours;
+      tprice.innerHTML = `Price: $ ${fprice}`;
+      fdepo = fprice * 0.5;
+      tdepo.innerHTML = `Deposit: $ ${fdepo}`;
+      console.log("4 hours charged",fprice);
+    }
+    else if(days <= 7){
+      tduration.innerHTML = `Duration: ${days} Days ${diffHoursMilliseconds} Hours`;
+      totalHours = days*24 + diffHoursMilliseconds;
+      fprice = sampleToolData[0].prices.hourly * totalHours * .9 ;
+      tprice.innerHTML = `Price: $ ${fprice}`;
+      fdepo = fprice * 0.5;
+      tdepo.innerHTML = `Deposit: $ ${fdepo}`;
+      console.log("day price charged",fprice, "total hours:", totalHours, "hours", diffHoursMilliseconds);
+    }
+    else if(days >= 7){
+      tduration.innerHTML = `Duration: ${days} Days ${diffHoursMilliseconds} Hours`;
+      totalHours = days*24 + diffHoursMilliseconds;
+      fprice = sampleToolData[0].prices.hourly * totalHours * .8 ;
+      tprice.innerHTML = `Price: $ ${fprice}`;
+      fdepo = fprice * 0.5;
+      tdepo.innerHTML = `Deposit: $ ${fdepo}`;
+      console.log("week price charged",fprice);
+    }
   }
+
+  nextbtn.addEventListener('click', ()=>{
+    page1.classList.add('shown');
+    page2.classList.remove('shown');
+    tname2.innerHTML = sampleToolData[0].toolName;
+    totalprice.innerHTML = `Price: $${fprice}`;
+    if (diffHoursMilliseconds != 0){
+    duration.innerHTML = `${days} days and ${diffHoursMilliseconds} hours`
+    }else{
+    duration.innerHTML = `${days} days`
+    }
+    rentdays.innerHTML =  `${dateinput1} to <br> ${dateinput2}`;
+    tnameloc.innerHTML = sampleToolData[0].toolName;
+    locationpicked.innerHTML ='pending' ;  //pending  to see how to set the location
+    payment.innerHTML = 'pending' ; //how to show the payment
+
+  })
+
+  reservebtn.addEventListener('click',()=>{
+    page2.classList.add('shown');
+    page3.classList.remove('shown');
+    
+  })
+
   /*
    * ============================================
    * Page Initialization
