31079fb9e64c634cc01161bb3d5009e521d2dfc9
Koichi <koichi89592@gmail.com>
2022-03-03 21:40:20 -0800

    Change header

--- Rentool/js/common.js
+++ Rentool/js/common.js
@@ -1,1 +1,7 @@
-console.log("Common.js");
+// SIDE MENU
+const toggle = document.getElementById('toggle');
+const sidemenu = document.getElementById('menu');
+
+toggle.onclick = function() {
+  sidemenu.classList.toggle('active');
+};
