44baccb0f98e915e2fe62cd15e6270bd9d6bf33c
Koichi <koichi89592@gmail.com>
2022-03-13 15:48:50 -0700

    Remove extra code from ToolListItem.js

--- Rentool/js/components/ToolListItem.js
+++ Rentool/js/components/ToolListItem.js
@@ -7,9 +7,7 @@ export class ToolListItem extends HTMLElement {
     this.render();
   }
   render() {
-    const { toolName, description, imageUrl, reservationToolIndex, isReserved } = this.tool;
-    console.log('isReserved: ', isReserved);
-    const EXCERPT_LENGTH = 50;
+    const { toolName, imageUrl, reservationToolIndex, isReserved } = this.tool;
 
     this.classList.add('tool-list__item');
     this.innerHTML = `
@@ -31,21 +29,3 @@ export class ToolListItem extends HTMLElement {
 }
 
 window.customElements.define('tool-list__item', ToolListItem);
-
-
-
-// this.innerHTML = `
-// <div class="tool-item__image-container">
-//   <img src="${imageUrl}" alt="${toolName}" class="tool-item__image">
-// </div>
-// <div class="tool-item__tool-name">${toolName}</div>
-// <div class="tool-item__description">${description.slice(0, EXCERPT_LENGTH)}</div>
-// <div class="tool-item__availability">
-//   <span class="circle">
-//   <i class="fa-solid fa-circle"></i></span><span class="tool-item__availability--text">Available at your location</span>
-// </div>
-// <form action="view-product.html" method="get">
-//   <input type="hidden" name="reservationToolIndex" value="${reservationToolIndex}">
-//   <input type="submit" value="Button" class="tool-item__submit">
-// </form>
-// `;
