aaa0415bb5f10f7ce4a24a00eef0d3301b0cd416
Koichi <koichi89592@gmail.com>
2022-03-13 21:32:57 -0700

    Add more-tool button

--- Rentool/js/product-result.js
+++ Rentool/js/product-result.js
@@ -5,6 +5,14 @@ import { getUrlParams, GET_PARAMS } from './util.js';
 const pageHeadingTextEl = document.getElementById('page-heading__text');
 pageHeadingTextEl.textContent = getUrlParams()[GET_PARAMS.CATEGORY] || getUrlParams()[GET_PARAMS.KEYWORD] || "Our Products";
 
+/**@type {HTMLDivElement} */
+const toolListEl = document.getElementById('tool-list');
+
+/**@type {HTMLButtonElement} */
+const moreToolsButton = document.getElementById('more-tools-button');
+
+const NUMBER_FIRST_VIEW_TOOL_ITEM_COUNT = 3;
+
 document.addEventListener('DOMContentLoaded', async () => {
   /**@type {[] | undefined} */
   let tools = undefined;
@@ -21,10 +29,20 @@ document.addEventListener('DOMContentLoaded', async () => {
     tools = await getAllTools();
   }
 
-  const toolListEl = document.getElementById('tool-list');
-  for (const tool of tools) {
-    const toolListItem = new ToolListItem(tool);
-    toolListEl.appendChild(toolListItem);
+
+  const numToolsCount = tools.length;
+  if (numToolsCount <= NUMBER_FIRST_VIEW_TOOL_ITEM_COUNT) {
+    showAllTool(tools);
+    moreToolsButton.style.display = 'none';
+  }
+
+  if (numToolsCount > NUMBER_FIRST_VIEW_TOOL_ITEM_COUNT) {
+    showFirstViewTool(tools);
+    moreToolsButton.style.display = 'block';
+    moreToolsButton.addEventListener('click', () => {
+      showMoreTool(tools);
+      moreToolsButton.style.display = 'none';
+    });
   }
 
   // Set the number of tools shown
@@ -32,3 +50,40 @@ document.addEventListener('DOMContentLoaded', async () => {
   // Remove loading spinner
   document.querySelector('.loader-container').style.display = 'none';
 });
+
+
+/**@param {Tool[]} tools */
+const showAllTool = (tools) => {
+  for (const tool of tools) {
+    appendToolListItemToToolList(tool);
+  }
+};
+
+/**@param {Tool[]} tools */
+const showFirstViewTool = (tools) => {
+  for (let i = 0; i < NUMBER_FIRST_VIEW_TOOL_ITEM_COUNT; i++) {
+    appendToolListItemToToolList(tools[i]);
+  }
+};
+
+/**
+ * @description Show additional tools when more button clicked
+ * @param {Tool[]} tools
+ * @param {number} startAd
+ * @param {number} endAt
+ */
+const showMoreTool = (tools, startAd = NUMBER_FIRST_VIEW_TOOL_ITEM_COUNT, endAt = tools.length) => {
+  for (let i = startAd; i < endAt; i++) {
+    appendToolListItemToToolList(tools[i]);
+  }
+};
+
+
+/**
+ * @description Append tool-item element to global tool-list
+ * @param {Tool} tool
+ */
+const appendToolListItemToToolList = (tool) => {
+  const toolListItem = new ToolListItem(tool);
+  toolListEl.appendChild(toolListItem);
+};
