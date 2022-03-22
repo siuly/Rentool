3c32a5cc7fb02e1e78c2059f95a4661bb77a3cfc
Koichi <koichi89592@gmail.com>
2022-03-21 21:37:22 -0700

    Change header making logo link to home

--- Rentool/js/components/RentoolHeader.js
+++ Rentool/js/components/RentoolHeader.js
@@ -17,21 +17,19 @@ export class RentoolHeader extends HTMLElement {
 
     this.innerHTML = `
     <header>
-      <img src="../images/logo-full.svg" alt="rentool-logo" class="logo">
+    <a href="./home.html" class="logo"><img src="../images/logo-full.svg" alt="rentool-logo" class="logo__image"></a>
       <h1><a href="./home.html">Rentool</a></h1>
       <div class="header-left">
         <button id="toggle"><i class="fas fa-bars"></i>
         </button>
         <nav id="menu">
           <ul>
-
             <li><a href="./active-orders.html">My Active Orders <i class="fa-solid fa-caret-right"></i></a></li>
             <li><a href="./catalogue.html">Tool Catalogue <i class="fa-solid fa-caret-right"></i></a></li>
             <li><a href="./user-profile.html">User Profile <i class="fa-solid fa-caret-right"></i></a></li>
             <li><a href="./services.html">Services <i class="fa-solid fa-caret-right"></i></a></li>
             <li><a href="./reference.html">Reference <i class="fa-solid fa-caret-right"></i></a></li>
             <li id="sign-status"><a href="" id="sign-out">Sign out</a></li>
-
           </ul>
         </nav>
       </div>
