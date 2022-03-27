9652dffb6653dbad8b4e56a13b3a05fffbc44f55
Koichi <koichi89592@gmail.com>
2022-03-26 23:37:51 -0700

    Change User.js structure

--- Rentool/js/user-profile.js
+++ Rentool/js/user-profile.js
@@ -20,9 +20,9 @@ const profileImage = document.getElementById('profile-image');
   }
 
   fullNameEl.textContent = user.fullName();
-  emailEl.textContent = user.email;
-  phoneNumberEl.textContent = user.phone;
-  addressEl.textContent = user.address;
+  emailEl.textContent = user.email === '' ? '-' : user.email;
+  phoneNumberEl.textContent = user.phone === '' ? '-' : user.phone;
+  addressEl.textContent = user.address === '' ? '-' : user.address;
   if (user.profileUrl !== '') {
     profileImage.src = user.profileUrl;
   }
